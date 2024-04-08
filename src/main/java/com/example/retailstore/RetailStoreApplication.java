bf9d555a15ac89407dbb22a066283f850c7c0712
powersystems013 <111869688+powersystems013@users.noreply.github.com>
2024-04-08 17:24:02 +0530

    Update RetailStoreApplication.java

--- src/main/java/com/example/retailstore/RetailStoreApplication.java
+++ src/main/java/com/example/retailstore/RetailStoreApplication.java
@@ -1,7 +1,7 @@
 package com.example.retailstore;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplicationnnnnnnnnnnn;
 
 @SpringBootApplication
 public class RetailStoreApplication {
